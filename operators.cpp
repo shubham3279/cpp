0dbc04b4306a5a1a17087b904f65a860afdb3316
Shubham Kumar <shubhamv0918@gmail.com>
2026-01-01 22:42:20 +0530

    Notes on C++ Operators

---
+++ operators.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+int main()
+{
+    // integer
+    int num1{2};
+    int num2{7};
+    int num3{0};
+    
+    // float and double
+    
+    // char
+    
+    // bool
+    
+    
+    
+    // BINARY OPERATORS
+        // DEFINITION: Binary Operators are those which operators on exactly two operands.
+        // TYPES: 
+            // 1. Arithmetic
+            // 3. Relational
+            // 2. Logical
+            // 3. Binary
+        // RESULTANT DATA TYPE: 
+            // All data types are essentuially numbers. So two binary operators can operate on two different data types as well.
+            //For a binary operator working on two different data type, the resulting data type depends on the ranking of different data types as well as range modifiers being used, as shown below:
+            // 1. long double
+            // 2. double
+            // 3. float
+            // 4. unsigned long long
+            // 5. long long
+            // 6. unsigned long
+            // 7. long
+            // 8. unsigned int
+            // 9. int
+            // 10. unsigned short
+            // 11. short
+            // 12. unsigned char
+            // 13. signed char
+            // 14. char
+            // 15. bool      
+            // Lower preference operand is converted to higher preference operand before operation.
+            
+
+    
+    // Addition Operator (+) : to get sum of two numeric data types
+        int sum = num1 + num2; // 2 + 7 = 9
+    
+    //
+        int sub = num1 - num2; // 2 - 7 = -5
+    
+    //
+        int mul = num1 * num2; // 2 * 7 = 14
+    
+    // 
+        int rem = num1 / num2; // 
+    
+}
